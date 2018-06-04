5995ae51cfc27cd937fdec4280d239e58fe0ffd0
Judithadela <adela_97_18@hotmail.com>
2018-06-03 20:23:19 -0500

    subiendo cambios

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,34 @@
+const encode = (offset,string) => {
+     let resultado = "";
+    for(let i = 0; i < string.length; i++) {
+        let caracter = string.charCodeAt(i);
+        if(caracter >= 97 && caracter <= 122) {
+            resultado += String.fromCharCode((caracter - 97 + offset) % 26 + 97);
+        } else if(caracter >= 65 && caracter <= 90) {
+          resultado += String.fromCharCode((caracter - 65 + offset) % 26 + 65);
+        } else {
+            resultado += string.charAt(i);
+        }
+    }
+    return resultado;
+}
+
+ const decode = (offset,string) => {
+  let resultado = "";
+  for(let i = 0; i < string.length; i++) {
+        let caracter = string.charCodeAt(i);
+        if(caracter >= 97 && caracter <= 122) {
+            resultado += String.fromCharCode((caracter + 97 - offset) % 26 + 97);
+        } else if(caracter >= 65 && caracter <= 90) {
+            resultado += String.fromCharCode((caracter + 65 - offset ) % 26 + 65);
+        } else {
+            resultado += string.charAt(i);
+        }
+    }
+    return resultado;
+}
+
 window.cipher = {
-  // ... 
+  encode:cifrar,
+  decode:descifrar
 };
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const contenido=document.getElementById('texto');
+const contenidoall=document.getElementById('texto2');
+const buttonCode=document.getElementById('buttonCode');
+const buttonDecode=document.getElementById('buttonDecode');
+//const desplazar=document.getElementById('change').value;
+const desplazar=parseInt(document.getElementById('change').value);
+contenido.addEventListener('input', () => {
+console.log(texto.value);
+})
+const cifrar = () => {
+  console.log();
+}
+buttonCode.addEventListener('click',buttonCode);
+
+contenidoall.addEventListener('input',() => {
+  console.log(texto2.value);
+})
+const descifrar = () => {
+  console.log();
+}
+buttonDecode.addEventListener('click',buttonDecode);
